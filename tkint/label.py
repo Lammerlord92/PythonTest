6b130b509a401952eb176b2ebbf46c6c1439982b
JavCarFer <javcarfer92@gmail.com>
2020-04-06 10:59:08 +0200

    Labels

--- tkint/label.py
+++ tkint/label.py
@@ -7,14 +7,23 @@ root.title("Hola mundo")
 root.resizable(1,1)
 root.iconbitmap('favicon.ico')
 
+# Variables dinámicas
+texto=StringVar()
+texto.set("Un nuevo texto")
 
 frame=Frame(root,width=480,height=200)
 frame.pack() #Alinea por defecto en el centro
 
 label=Label(frame, text="¡Hola mundo!")
-label.place(x=0,y=0)
+label.pack()
+label.config(fg="blue",font={"Verdana",24})
 
+label2=Label(frame, text="¡Hola mundo!")
+label2.pack()
+label2.config(fg="red",font={"Verdana",24})
+label2.config(textvariable=texto)
 
-
+image=PhotoImage(file="Different-phases-of-map-reduce-in-word-count-example.png")
+Label(root,image=image).pack()
 # bucle de la aplicación
 root.mainloop()
